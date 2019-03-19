1d93df38038e562a37135d2d00dab3665ca0d9bc
BTP_Souradip&Abhishek <souradip@iitg.ac.in>
2019-03-19 22:29:53 +0530

    Key generation code done.

--- pqc_codes/generate_keys/triple_pendulum.m
+++ pqc_codes/generate_keys/triple_pendulum.m
@@ -8,9 +8,30 @@ t = linspace(0,duration,nframes+1);
 y=sol(1:nframes+1,:)';
 Y=sol(1:nframes+1,1:3)';
 
-figure;
-plot(t,Y);
-grid on
+% figure;
+% plot(t,Y);
+% grid on
+% xlabel('Time(t)[seconds]');ylabel('Theta[rad]');
+% legend('theta1','theta2','theta3')
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+% phi1=y(1,:)'; dtphi1=y(4,:)';
+% phi2=y(2,:)'; dtphi2=y(5,:)';
+% phi3=y(3,:)'; dtphi3=y(6,:)';
+% l1=ivp(10); l2=ivp(11); l3=ivp(12);
+% 
+% figure;
+% hold on;
+% xlabel('X');ylabel('Y');
+% axis ij;
+% grid on
+% plot(l1*sin(phi1),-l1*cos(phi1));
+% plot(l1*sin(phi1)+l2*sin(phi2),-l1*cos(phi1)-l2*cos(phi2));
+% plot(l1*sin(phi1)+l2*sin(phi2)+l3*sin(phi3),-l1*cos(phi1)-l2*cos(phi2)-l3*cos(phi3));
+% legend('theta1','theta2','theta3')
+% hold off;
+% pause;
+%%%%%%%%%%%%%%%%%%%%
 
 Y1=Y(1,:);
 %Y1 = Y1 - (floor(Y1./(2*pi)).*(2*pi));
@@ -19,16 +40,19 @@ Y2=Y(2,:);
 Y3=Y(3,:);
 %Y3 = Y3 - (floor(Y3./(2*pi)).*(2*pi));
 
+% Y = Y - (floor(Y./(2*pi)).*(2*pi));
+% figure;
+% plot(t,Y(3,:));
+% grid on
+% pause;
+
+
 [pval1, fMax1] = test_periodicity(Y1);
 [pval2, fMax2] = test_periodicity(Y2);
 [pval3, fMax3] = test_periodicity(Y3);
 
-% figure;
-% plot(t,Y1,Y2,Y3);
-% grid on
-pause
 
-eps=0.0005;
+eps=0.005;
 if fMax1>eps||fMax2>eps||fMax3>eps
     fprintf("%f,%f,%f->Periodic\n",fMax1,fMax2,fMax3);
 else
@@ -46,7 +70,7 @@ else
                 possibles = [possibles,j];
             end
         end
-        if length(possibles)==0
+        if isempty(possibles)
             flag = 0;
             fprintf("%d\n",i);
             break;
@@ -54,7 +78,7 @@ else
         intervals{end+1} = possibles;
     end
     if flag == 1
-        fprintf("Accepted");
+        fprintf("Accepted\n");
         dlmwrite('key_test.csv',ivp','delimiter',',','-append');
     end
 end
