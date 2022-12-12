cc13aa6a667d7ead994fb57030339da51a3fadc0
souradip_pal <souradip.iitg@gmail.com>
2022-12-12 03:51:12 -0500

    Some tests.

--- cryptosystem_test/diffCrypt.m
+++ cryptosystem_test/diffCrypt.m
@@ -20,11 +20,11 @@ key_len = length(key);
 m1 = 0:255;
 c1 = encryption(m1, key); 
 
-s1 = [];
-for i=0:255
-    f = halfprecision(key(mod(i, key_len)+1,1));
-    s1 = [s1, bitxor(c1(i+1), f)];
-end
+% s1 = [];
+% for i=0:255
+%     f = halfprecision(key(mod(i, key_len)+1,1));
+%     s1 = [s1, bitxor(c1(i+1), f)];
+% end
 
 % b = [];
 % for i=0:255
@@ -73,34 +73,55 @@ end
 % figure;
 % H_delC3 = histogram(delC3, 256);
 
-m4 = [];
-for i=0:255
-    m4 = [m4, mod(2*i,256)]; %%% change m4 accordingly to set delta M
+% m4 = [];
+% for i=0:255
+%     m4 = [m4, mod(2*i,256)]; %%% change m4 accordingly to set delta M
+% end
+% 
+% c4 = encryption(m4, key);
+% 
+% delC4 = [];
+% for i=1:256
+%     %disp({c1(i), c4(i), abs(int32(c1(i)) - int32(c4(i)))});
+%     delC4 = [delC4, mod(abs(int32(c1(i)) - int32(c4(i))), 65536)];
+% end
+% 
+% figure;
+% H_delC4 = histogram(delC4, 65536);
+% 
+% 
+% s4 = [];
+% for i=0:255
+%     f = halfprecision(key(mod(i, key_len)+1,1));
+%     s4 = [s4, bitxor(c4(i+1), f)];
+% end
+% 
+% delS4 = [];
+% for i=1:256
+%     %disp({s1(i), s4(i), abs(int32(s1(i)) - int32(s4(i)))});
+%     delS4 = [delS4, mod(abs(int32(s1(i)) - int32(s4(i))), 65536)];
+% end
+% 
+% figure;
+% H_delS4 = histogram(delS4, 65536);
+
+
+diff_dist = zeros(256, 65536);
+for i = 1:256
+    for j = 1:256
+        X1 = m1(i);
+        X2 = m1(j);
+        Y1 = c1(i);
+        Y2 = c1(j);
+%         deltaX = bitxor(X1,X2);
+%         deltaY = bitxor(Y1,Y2);
+        deltaX = mod(X1-X2,256);
+        deltaY = mod(Y1-Y2,65536);
+        fprintf("%d, %d, %d\n", X1,X2, deltaX);
+        fprintf("%d, %d, %d\n", Y1,Y2, deltaY);
+        diff_dist(deltaX+1, deltaY+1) = diff_dist(deltaX+1,deltaY+1)+1;
+    end
 end
 
-c4 = encryption(m4, key);
+csvwrite('diff_dist1.csv',diff_dist);
 
-delC4 = [];
-for i=1:256
-    %disp({c1(i), c4(i), abs(int32(c1(i)) - int32(c4(i)))});
-    delC4 = [delC4, mod(abs(int32(c1(i)) - int32(c4(i))), 65536)];
-end
-
-figure;
-H_delC4 = histogram(delC4, 65536);
-
-
-s4 = [];
-for i=0:255
-    f = halfprecision(key(mod(i, key_len)+1,1));
-    s4 = [s4, bitxor(c4(i+1), f)];
-end
-
-delS4 = [];
-for i=1:256
-    %disp({s1(i), s4(i), abs(int32(s1(i)) - int32(s4(i)))});
-    delS4 = [delS4, mod(abs(int32(s1(i)) - int32(s4(i))), 65536)];
-end
-
-figure;
-H_delS4 = histogram(delS4, 65536);
--- simulate_pendulum/main.m
+++ simulate_pendulum/main.m
@@ -6,17 +6,18 @@ sym theta
 duration=6.5535;
 tspan = [0 duration];
 
+global m1 m2 m3 L1 L2 L3 k1 k2 k3 I1 I2 I3 g;
 m1=0.2944;m2=0.1756;m3=0.0947;
-l1=0.508;l2=0.254;l3=0.127;
+L1=0.508;L2=0.254;L3=0.127;
 k1=0.005;k2=0;k3=0.0008;
 I1=9.526e-3;I2=1.625e-3;I3=1.848e-4;
 g=9.81;
 
-params=[m1,m2,m3,l1,l2,l3,I1,I2,I3,k1,k2,k3,g];
+params=[m1,m2,m3,L1,L2,L3,I1,I2,I3,k1,k2,k3,g];
 
-theta1 = 2*pi-0.4603;
-theta2 = 2*pi-1.2051;
-theta3 = 2*pi-1.5165;
+theta1 = -0.4603;
+theta2 = -1.2051;
+theta3 = -1.5165;
 dtheta1 = 0;
 dtheta2 = 0;
 dtheta3 = 0;
