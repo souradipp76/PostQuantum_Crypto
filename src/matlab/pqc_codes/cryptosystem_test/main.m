099cc4429a0019a552f36ba149c099ed6288ac94
souradip_pal <souradip.iitg@gmail.com>
2022-12-20 02:51:01 -0500

    Refactored directory structure.

---
+++ src/matlab/pqc_codes/cryptosystem_test/main.m
@@ -0,0 +1,46 @@
+clc;
+clear;
+
+%%%%%%%%% Key %%%%%%%%%%
+m1=0.2944;m2=0.1756;m3=0.0947;
+l1=0.508;l2=0.254;l3=0.127;
+k1=0.005;k2=0;k3=0.0008;
+I1=9.526e-3;I2=1.625e-3;I3=1.848e-4;
+g=9.81;
+
+params=[m1,m2,m3,l1,l2,l3,I1,I2,I3,k1,k2,k3,g];
+
+theta1 = 2*pi-0.46031;
+theta2 = 2*pi-1.2051;
+theta3 = 2*pi-1.5165;
+dtheta1 = 0;
+dtheta2 = 0;
+dtheta3 = 0;
+
+iniCon = [theta1; theta2; theta3; dtheta1; dtheta2; dtheta3];
+
+key = [iniCon;params']; 
+
+%%%%%%%%% Input %%%%%%%%
+%str = 'A course in Cryptography';
+
+%%%%%%%%%%%%%%%%%%Example Text%%%%%%%%%%%%%%%%%%%%%
+%test_text('data/text/sample.txt', key);
+
+%%%%%%%%%%%%%%%%%% Example Image %%%%%%%%%%%%%%%%%%%%%
+%test_image('data/images/leena.tif', key);
+
+
+%%%%%%%%%%%%%%%%% Image Analysis %%%%%%%%%%%%%%%%%%%%%%%%
+%graphic_corr('data/images/leena.tif', key);
+
+%calculate_entropy('data/images/leena.tif', key);
+
+%[NPCR, UACI] = calculate_NPCR_UACI('data/images/leena.tif', key);
+
+%test_occlusion('data/images/leena.tif', key);
+
+%diffCrypt(key);
+
+%cpa('data/images/leena.tif', key);
+
